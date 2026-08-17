dd5461e88a3b55a21d569b366bb7fe498e8490e7
scriptfenio <121816368+scriptfenio@users.noreply.github.com>
2026-08-17 05:22:07 +0200

    chore: update base path in vite config
    Adjust base path to /Sharing-Knowledge/ to ensure correct asset loading for production deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Sharing-Knowledge/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
